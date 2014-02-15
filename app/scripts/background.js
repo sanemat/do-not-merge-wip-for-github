f42e66a3769f2bfa5eea0f67295a14bdccacacc8
sanemat <o.gata.ken@gmail.com>
2014-02-15 20:16:58 +0900

    Use 2 space indent

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,5 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+  console.log('previousVersion', details.previousVersion);
 });
