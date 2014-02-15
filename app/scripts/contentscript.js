370676fbefb12fc56280f6e19d9a747802df7feb
sanemat <o.gata.ken@gmail.com>
2014-02-16 02:03:53 +0900

    Add You can't merge! logic

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,5 +1,11 @@
 'use strict';
 
 (function($){
-  $('#js-discussion-header').text('foo bar');
+  var $container = $('#js-repo-pjax-container');
+  var issueTitle = $container.find('#js-discussion-header .js-issue-title').text();
+  if (/(\[wip\]|\[do\s*not\s*merge\])/i.test(issueTitle)) {
+    var $buttonMerge = $container.find('#js-pull-merging button.merge-branch-action.js-details-target');
+    $buttonMerge.attr('disabled', true);
+    $buttonMerge.text("WIP! You can't merge!");
+  }
 })(jQuery);
