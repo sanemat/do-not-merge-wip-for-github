aa87f92e3aa20688a5b36dbf573422758bbf0dd5
fukayatsu <fukayatsu@gmail.com>
2014-06-25 19:53:31 +0900

    Check pull-request-status at intervals.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,11 +1,14 @@
 'use strict';
 
 (function($){
-  var $container = $('#js-repo-pjax-container');
-  var issueTitle = $container.find('#js-discussion-header .js-issue-title').text();
-  if (/(\[wip\]|\[do\s*not\s*merge\])/i.test(issueTitle)) {
+  var changeMergeButtonState = function() {
+    var $container = $('#js-repo-pjax-container');
+    var issueTitle = $container.find('#js-discussion-header .js-issue-title').text();
+    var caution = /(\[wip\]|\[do\s*not\s*merge\])/i.test(issueTitle);
     var $buttonMerge = $container.find('#js-pull-merging button.merge-branch-action.js-details-target');
-    $buttonMerge.attr('disabled', true);
-    $buttonMerge.text("WIP! You can't merge!");
-  }
+    var buttonText = caution ? 'WIP! You can\'t merge!' : 'Merge pull request';
+    $buttonMerge.attr('disabled', caution);
+    $buttonMerge.text(buttonText);
+  };
+  setInterval(changeMergeButtonState, 1000);
 })(jQuery);
