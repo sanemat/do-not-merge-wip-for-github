83e26ca8acc022f07bf0486afce73ba85dc2e438
dtaniwaki <daisuketaniwaki@gmail.com>
2014-07-02 20:29:33 +0900

    Protect branches from deleting

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,3 +3,9 @@
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  if (request.subject === 'localStorage') {
+    sendResponse({localStorage: localStorage});
+  }
+});
