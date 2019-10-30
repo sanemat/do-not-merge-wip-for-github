c94c3587d7c9d0afe52c15ae4674c3346e436a50
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-10-30 23:28:00 +0700

    chore(options): use camel case for id and name

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -3,12 +3,12 @@
 
   window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('protectedBranch').value = localStorage.protectedBranch;
-    document.getElementById('button_message').value = localStorage.buttonMessage;
+    document.getElementById('buttonMessage').value = localStorage.buttonMessage;
 
     document.getElementById('save_btn').closest('form').addEventListener('submit', e => {
       e.preventDefault();
       localStorage.protectedBranch = e.target.protectedBranch.value;
-      localStorage.buttonMessage = e.target.button_message.value;
+      localStorage.buttonMessage = e.target.buttonMessage.value;
 
       window.alert('The options have been saved!');
     });
