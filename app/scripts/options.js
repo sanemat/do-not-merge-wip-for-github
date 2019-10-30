9bd5998e266c271f5529e0cc0cad7388385fa1e9
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-10-30 23:20:51 +0700

    chore(options): use single quote

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -2,8 +2,8 @@
   'use strict';
 
   window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("protected_branch").value = localStorage.protectedBranch;
-    document.getElementById("button_message").value = localStorage.buttonMessage;
+    document.getElementById('protected_branch').value = localStorage.protectedBranch;
+    document.getElementById('button_message').value = localStorage.buttonMessage;
 
     document.getElementById('save_btn').closest('form').addEventListener('submit', e => {
       e.preventDefault();
