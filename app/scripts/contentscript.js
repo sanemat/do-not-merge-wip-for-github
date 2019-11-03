28b1571b16162b10a9af7a254a2778ec2d6422b1
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-11-03 16:29:07 +0700

    refactor(content): use let or const

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,35 +2,35 @@
   'use strict';
 
   function changeMergeButtonState() {
-    var container = document.querySelector('#js-repo-pjax-container');
-    var issueTitle = container.querySelector('.js-issue-title').textContent;
-    var buttonMerges = container.querySelectorAll('.merge-message button[data-details-container]');
-    var buttonMergeOptions = container.querySelectorAll('.merge-message button[data-details-container] + .select-menu-button');
-    var disabled = false;
-    var buttonHtml = '';
+    let container = document.querySelector('#js-repo-pjax-container');
+    let issueTitle = container.querySelector('.js-issue-title').textContent;
+    let buttonMerges = container.querySelectorAll('.merge-message button[data-details-container]');
+    let buttonMergeOptions = container.querySelectorAll('.merge-message button[data-details-container] + .select-menu-button');
+    let disabled = false;
+    let buttonHtml = '';
 
     chrome.runtime.sendMessage({from: 'content', subject: 'localStorage'}, function(response){
       if (!response) { return; }
 
-      var localStorage = response.localStorage;
-      var wipTitleRegex = /[\[(^](do\s*n[o']?t\s*merge|wip|dnm)[\]):]/i;
-      var wipTagRegex = /(wip|do\s*not\s*merge|dnm)/i;
+      const localStorage = response.localStorage;
+      const wipTitleRegex = /[\[(^](do\s*n[o']?t\s*merge|wip|dnm)[\]):]/i;
+      const wipTagRegex = /(wip|do\s*not\s*merge|dnm)/i;
 
-      var isWipTitle = wipTitleRegex.test(issueTitle);
-      var isWipTaskList = container.querySelector('.timeline-comment') && container.querySelector('.timeline-comment').querySelector('input[type="checkbox"]:not(:checked)') !== null;
-      var isSquashCommits = false;
+      const isWipTitle = wipTitleRegex.test(issueTitle);
+      const isWipTaskList = container.querySelector('.timeline-comment') && container.querySelector('.timeline-comment').querySelector('input[type="checkbox"]:not(:checked)') !== null;
+      let isSquashCommits = false;
       for (const commitMessage of container.querySelectorAll('.commit-message')) {
         isSquashCommits = isSquashCommits || commitMessage.textContent.match(/(squash|fixup)!/);
       }
 
-      var isWipTag = false;
+      let isWipTag = false;
       for (const label of container.querySelectorAll('.js-issue-labels .IssueLabel')) {
         isWipTag = isWipTag || label.textContent.match(wipTagRegex);
       }
 
       disabled = (isWipTitle || isWipTaskList || isSquashCommits || isWipTag);
 
-      var buttonMessage = '';
+      let buttonMessage = '';
 
       if (localStorage && localStorage.buttonMessage) {
         buttonMessage = localStorage.buttonMessage;
